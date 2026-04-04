02afa2e0a806d1d09e39ea918ea67e2dfc09ec96
mohammedkhamrich <mohammedkhamrich17@gmail.com>
2026-04-04 20:38:39 +0200

    Delete code directory

--- code/order-service/src/main/java/com/ecommerce/order_service/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ecommerce.order_service.repository;
-
-import com.ecommerce.order_service.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {}
